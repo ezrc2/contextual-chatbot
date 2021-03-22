dc81ce2519341289b3eb629402c8038d0fee8fbb
Eric Chen <ezchen2556@gmail.com>
2021-03-22 11:07:01 -0700

    Add nn layers and forward function

--- model.py
+++ model.py
@@ -1,8 +1,8 @@
 import torch
 import torch.nn as nn
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset
 
-class Chatbot(Dataset):
+class ChatbotData(Dataset):
     
     def __init__(self, X_train, y_train):
         self.X_train = X_train
@@ -14,3 +14,23 @@ class Chatbot(Dataset):
 
     def __len__(self):
         return self.num_samples
+
+
+class NeuralNetwork(nn.Module):
+
+    def __init__(self, input_size, hidden_size, num_classes):
+        super(NeuralNetwork, self).__init__()
+        self.lin1 = nn.Linear(input_size, hidden_size)
+        self.lin2 = nn.Linear(hidden_size, hidden_size)
+        self.lin3 = nn.Linear(hidden_size, num_classes)
+
+    def forward(self, x):
+        x = self.lin1(x)
+        x = nn.ReLU(x)
+        x = self.lin2(x)
+        x = nn.ReLU(x)
+        x = self.lin3(x)
+        return x
+
+    def train(self):
+        pass
