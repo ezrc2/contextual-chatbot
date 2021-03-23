60f625b13f60156329667b9c47cf5f75b7223aa6
Eric Chen <ezchen2556@gmail.com>
2021-03-23 12:56:20 -0700

    Add chatbot loop and code to choose a response

--- chatbot.py
+++ chatbot.py
@@ -1,20 +1,52 @@
 import json
-import json
+import random
 import torch
 import nltk
+from dataset import bag_of_words
 from model import NeuralNetwork
 
 with open('intents.json') as file:
     intents = json.load(file)
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model_data = torch.load("model.pth")
-input_size = model_data["input_size"]
-hidden_size = model_data["hidden_size"]
-output_size = model_data["output_size"]
+model_data = torch.load('model.pth')
+input_size = model_data['input_size']
+hidden_size = model_data['hidden_size']
+output_size = model_data['output_size']
 all_words = model_data['all_words']
-tags = model_data['tags']
-model_state = model_data["model_state"]
+classes = model_data['classes']
+model_state = model_data['model_state']
 
 
-model = NeuralNetwork(input_size, hidden_size, output_size)
+model = NeuralNetwork(input_size, hidden_size, output_size).to(device)
+model.load_state_dict(model_state)
+model.eval()
+
+
+if __name__ == '__main__':
+    print("Hi, I'm eric-bot. Type 'quit' to exit")
+    while True:
+        sentence = input('> ')
+        if sentence == 'quit':
+            break
+
+        tokenized = nltk.word_tokenize(sentence)
+        X = bag_of_words(tokenized, all_words)
+        X = X.reshape(1, X.shape[0])
+        X = torch.from_numpy(X).to(device)
+
+        output = model(X)
+        _, pred = torch.max(output, dim=1)
+        tag = classes[pred.item()]
+
+        print(pred)
+
+        probs = torch.softmax(output, dim=1)
+        prob = probs[0][pred.item()]
+
+        if prob.item() > 0.75:
+            for intent in intents['intents']:
+                if tag == intent["tag"]:
+                    print(random.choice(intent['responses']))
+        else:
+            print("Sorry, I do not understand.")
